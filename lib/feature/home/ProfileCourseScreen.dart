9bb2ee3d81de40ac35f8aeafea2dcfa3babb6dea
Shraboni Akter <shrabonbhuiyan708@gmail.com>
2025-12-06 13:07:15 +0600

    code

--- lib/feature/home/ProfileCourseScreen.dart
+++ lib/feature/home/ProfileCourseScreen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,36 +9,15 @@ class ProfileCourseScreen extends StatefulWidget {
 class _ProfileCourseScreenState extends State<ProfileCourseScreen> {
   String currentSchool = "Diocesan Boys' School";
   String electiveSubject = "Diocesan Boys' School";
+
   String currentGrade = "Primary 5";
   String hearAboutUs = "Friends";
 
+  bool gradeExpanded = false;
+  bool hearExpanded = false;
+
   final List<String> grades = ["Primary 5", "Primary 6", "Form 1", "Form 2"];
   final List<String> hearList = ["Friends", "Facebook", "YouTube", "School"];
-
-  void openPicker(List<String> items, String currentValue, Function(String) onSelect) {
-    showModalBottomSheet(
-      context: context,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-      ),
-      builder: (_) {
-        return Container(
-          height: 250,
-          child: CupertinoPicker(
-            backgroundColor: Colors.white,
-            itemExtent: 40,
-            scrollController: FixedExtentScrollController(
-              initialItem: items.indexOf(currentValue),
-            ),
-            onSelectedItemChanged: (index) {
-              onSelect(items[index]);
-            },
-            children: items.map((e) => Center(child: Text(e))).toList(),
-          ),
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,159 +26,200 @@ class _ProfileCourseScreenState extends State<ProfileCourseScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Personalizing course\naccording to your profile",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-              ),
-              SizedBox(height: 20),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Personalizing course\naccording to your profile",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
 
-              // Current School
-              buildLabel("Current School"),
-              TextFormField(
-                decoration: InputDecoration(
-                  //labelText: "Email",
-                  hintText: 'Diocesan Boys School',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
+                SizedBox(height: 20),
 
-                    borderRadius: BorderRadius.circular(12.r),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.r),
-                    borderSide: BorderSide(
-                      color: Color(0xffFFFFFF) ,// Border color
-                      width: 1.5.h,
+                // Current School
+                buildLabel("Current School"),
+                buildTextField(currentSchool),
+
+                SizedBox(height: 20),
+
+                // Elective
+                buildLabel("Elective Subject"),
+                buildTextField(electiveSubject),
+
+                SizedBox(height: 20),
+
+                // CURRENT GRADE
+                buildLabel("Current Grade"),
+                buildExpandableDropdown(
+                  selected: currentGrade,
+                  expanded: gradeExpanded,
+                  list: grades,
+                  onTap: () {
+                    setState(() {
+                      gradeExpanded = !gradeExpanded;
+                      hearExpanded = false; // close other dropdown
+                    });
+                  },
+                  onSelect: (value) {
+                    setState(() {
+                      currentGrade = value;
+                      gradeExpanded = false;
+                    });
+                  },
+                ),
+
+                SizedBox(height: 20),
+
+                // HEAR ABOUT US
+                buildLabel("How Did You Hear About Us ?"),
+                buildExpandableDropdown(
+                  selected: hearAboutUs,
+                  expanded: hearExpanded,
+                  list: hearList,
+                  onTap: () {
+                    setState(() {
+                      hearExpanded = !hearExpanded;
+                      gradeExpanded = false; // close other dropdown
+                    });
+                  },
+                  onSelect: (value) {
+                    setState(() {
+                      hearAboutUs = value;
+                      hearExpanded = false;
+                    });
+                  },
+                ),
+
+                SizedBox(height: 40),
+
+                SizedBox(
+                  width: 400,
+                  height: 56.h,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff1153A0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.r),
-                    borderSide: BorderSide(
-                      color: Color(0xffF1F4F8),
-                      width: 1.5.w,
+                    onPressed: () {},
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(fontSize: 16.spMin, color: Colors.white),
                     ),
                   ),
                 ),
-
-              ),
-
-
-              SizedBox(height: 20),
-
-              // Elective
-              buildLabel("Elective Subject"),
-              TextFormField(
-                decoration: InputDecoration(
-                  //labelText: "Email",
-                  hintText: 'Diocesan Boys School',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-
-                    borderRadius: BorderRadius.circular(12.r),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.r),
-                    borderSide: BorderSide(
-                      color: Color(0xffFFFFFF) ,// Border color
-                      width: 1.5.h,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.r),
-                    borderSide: BorderSide(
-                      color: Color(0xffF1F4F8),
-                      width: 1.5.w,
-                    ),
-                  ),
-                ),
-
-              ),
-
-
-              SizedBox(height: 20),
-
-              // Grade
-              buildLabel("Current Grade"),
-              buildDropdownBox(currentGrade, () {
-                openPicker(grades, currentGrade, (value) {
-                  setState(() => currentGrade = value);
-                });
-              }),
-
-              SizedBox(height: 20),
-
-              // Hear about us
-              buildLabel("How Did You Hear About Us ?",),
-              buildDropdownBox(hearAboutUs, () {
-                openPicker(hearList, hearAboutUs, (value) {
-                  setState(() => hearAboutUs = value);
-                });
-              }),
-
-              Spacer(),
-              SizedBox(
-                width: 400,
-                height: 56.h,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff1153A0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "Get Started",
-                    style: TextStyle(fontSize: 16.spMin, color: Colors.white),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
+  // LABEL
   Widget buildLabel(String text) {
-    return Text(
-      text,
-      style: TextStyle(color: Colors.grey[700], fontSize: 14),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 6),
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.grey[700], fontSize: 14),
+      ),
     );
   }
 
-  Widget buildTextBox(String value) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
+  // NORMAL TEXT FIELD
+  Widget buildTextField(String value) {
+    return TextFormField(
+      initialValue: value,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        hintText: value,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.r),
+          borderSide: BorderSide(color: Color(0xffFFFFFF), width: 1.5),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.r),
+          borderSide: BorderSide(color: Color(0xffF1F4F8), width: 1.5),
+        ),
       ),
-      child: Text(value, style: TextStyle(fontSize: 15)),
     );
   }
 
-  Widget buildDropdownBox(String value, VoidCallback onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
+  // EXPANDABLE DROPDOWN
+  Widget buildExpandableDropdown({
+    required String selected,
+    required bool expanded,
+    required List<String> list,
+    required Function() onTap,
+    required Function(String) onSelect,
+  }) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 14, vertical: 18),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: Colors.grey.shade300),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(selected),
+                Icon(
+                  expanded
+                      ? Icons.keyboard_arrow_up
+                      : Icons.keyboard_arrow_down,
+                ),
+              ],
+            ),
+          ),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(value, style: TextStyle(fontSize: 15)),
-            Icon(Icons.keyboard_arrow_down)
-          ],
-        ),
-      ),
+
+        // EXPANDED LIST WITH DIVIDERS
+        if (expanded)
+          Container(
+            margin: EdgeInsets.only(top: 6),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: Colors.grey.shade300),
+            ),
+            child: Column(
+              children: [
+                for (int i = 0; i < list.length; i++) ...[
+                  InkWell(
+                    onTap: () => onSelect(list[i]),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      alignment: Alignment.center,
+                      child: Text(list[i]),
+                    ),
+                  ),
+
+                  if (i != list.length - 1)
+                    Divider(
+                      height: 1,
+                      thickness: 1,
+                      color: Colors.grey.shade200,
+                      indent: 0,
+                      endIndent: 0,
+                    ),
+                ]
+              ],
+            ),
+          ),
+      ],
     );
   }
 }
